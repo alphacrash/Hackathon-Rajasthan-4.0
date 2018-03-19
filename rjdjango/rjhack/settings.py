539a3891779bed0574e0955b084fe562be5ff504
alphacrash <sudhansu.ace@gmail.com>
2018-03-19 23:38:09 +0530

    Added static support to Django

--- rjdjango/rjhack/settings.py
+++ rjdjango/rjhack/settings.py
@@ -38,9 +38,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
-    #Custom App
+    # Custom App
     'land',
-    
+
     'healthcare',
 ]
 
@@ -123,5 +123,15 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static"),
+    # '/var/www/static/',
+]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'static_cdn')
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media_cdn')
+
 LOGIN_REDIRECT_URL = 'home'
-LOGOUT_REDIRECT_URL ='home'
+LOGOUT_REDIRECT_URL = 'home'
--- rjdjango/rjhack/urls.py
+++ rjdjango/rjhack/urls.py
@@ -13,15 +13,22 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic.base import TemplateView
 
 urlpatterns = [
-    path('', TemplateView.as_view(template_name='home.html'),name='home'),
+    path('', TemplateView.as_view(template_name='home.html'), name='home'),
     path('admin/', admin.site.urls),
     path('health/', include('healthcare.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
-
-    
 ]
+
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL,
+                          document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
